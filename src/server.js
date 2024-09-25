d11730791e3b23bd1cee7215bbebf85dad024e1d
Kássio <kbc1@aluno.ifal.edu.br>
2024-09-25 16:32:54 -0300

    aula 2

--- src/server.js
+++ src/server.js
@@ -21,17 +21,36 @@ import http from 'node:http'
 //GET /users - Buscando usuarios do back-end
 //POST /users - Criar um usuario no back-end
 
+//Stateful - Stateless
+
+//JSON - JavaScript Object Notation
+
+//Cabeçalhos (Requisição/resposta) => Metadados
+
+
+ 
+const users = []
+
 const server = http.createServer((req, res) =>{
   const {method, url} = req
 
-  if(method == "GET" && url == '/users') {
-    return res.end('Listagem de usuários')
+  if (method == 'GET' && url == '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+  
+  if (method == 'POST' && url == '/users') {
+    users.push({
+      id: 1,
+      name: 'Kassio bezerra',
+      email: 'kassiobcunha@gmail.com',
+    })
+
+    return res.writeHead(201).end()
   }
 
-  if(method == "POST" && url == '/users') {
-    return res.end('Criação de usuário')
-  }
-  return res.end('HELLO WORLD')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
