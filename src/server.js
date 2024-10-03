d932a6b310198bb7391f547c4827be64d2b4a1fd
Kássio <kbc1@aluno.ifal.edu.br>
2024-10-03 17:10:08 -0300

    aula 5

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import http from 'node:http'
-
+import { routes } from './routes.js'
 import { json } from './middlewares/json.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const server = http.createServer(async(req, res) =>{
   const {method, url} = req
 
   await json(req, res)
   
+  const route = routes.find(route => {
+    return route.method == method && route.path.test(url)
+  })
 
+  if (route) {
+    return route.handler(req, res)
+  }
+  
   return res.writeHead(404).end()
 })
 
